b0831555ab63d79d92cc1959070e89340cb2ed9b
Your GitHub Username <angelangelis314@gmail.com>
2025-05-16 00:02:53 -0700

    indivudual activity

--- src/js/product.js
+++ src/js/product.js
@@ -1,7 +1,28 @@
 import { setLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
+import { qs } from "./utils.mjs";
 
 const dataSource = new ProductData("tents");
+
+async function loadProductListing() {
+  const tentContainer = qs("#tent-list");
+  const tents = await dataSource.getData();
+
+  tents.forEach(tent => {
+    if (tent.hasDetailPage) {
+      const tentElement = document.createElement("li");
+      tentElement.classList.add("product-card");
+      tentElement.innerHTML = `
+      <a href="product_pages/${tent.Id}.html">
+        < img src="${tent_Image}" alt="${tent.Name}" />
+        <h3 class="card_brand">${tent.Brand.Name}</h3>
+        <h2 class="card_name">${tent.NameWithoutBrand}</h2>
+        <p class="product-card_price">$${tent.FinalPrice}</p>
+        </a>`;
+        tentContainer.appendChild(tentElement);
+    }
+  });
+}
 
 function addProductToCart(product) {
   setLocalStorage("so-cart", product);
@@ -11,6 +32,8 @@ async function addToCartHandler(e) {
   const product = await dataSource.findProductById(e.target.dataset.id);
   addProductToCart(product);
 }
+
+document.addEventListener("DOMContentLoaded", loadProductListing);
 
 // add listener to Add to Cart button
 document
